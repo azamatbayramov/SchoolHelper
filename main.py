158b5c4da3387478d70cb07046c94cb2549f6f20
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-03-01 22:36:40 +0500

    Add NotGroupMemberFilter filter

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from vkwave.bots import SimpleLongPollBot
+from filters import NotGroupMemberFilter
 from all_json import SETTINGS
 
 bot = SimpleLongPollBot(tokens=SETTINGS["token"], group_id=SETTINGS["group_id"])
 
 
-# Test
-@bot.message_handler()
-def handle_message(event):
-    return "Hello! I am bot!"
+@bot.message_handler(NotGroupMemberFilter())
+async def not_group_member_handler(event):
+    return "Упс! Походу ты не подписан на группу. Подпишись и возвращайся"
 
 
 bot.run_forever()
