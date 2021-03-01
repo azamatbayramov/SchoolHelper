56c08fb5b72137badd02425dcc18748d204fca71
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-03-01 20:18:05 +0500

    Add schedule

--- all_json.py
+++ all_json.py
@@ -1,3 +1,4 @@
 import json
 
 SETTINGS = json.load(open("json/settings.json", encoding="utf8"))
+SCHEDULE = json.load(open("json/schedule.json", encoding="utf8"))
--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from all_json import SETTINGS
 
 bot = SimpleLongPollBot(tokens=SETTINGS["token"], group_id=SETTINGS["group_id"])
 
+
+# Test
+@bot.message_handler()
+def handle_message(event):
+    return "Hello! I am bot!"
+
+
+bot.run_forever()
